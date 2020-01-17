13237d046213ae7d2f085fbbabc6f43791e0c660
estydaven <esetnod1111@gmail.com>
2020-01-17 13:35:59 +0300

    Карточка товара, без адаптива

---
+++ js/app.js
@@ -0,0 +1,44 @@
+$(window).load(function() {
+    $('#carousel').flexslider({
+      animation: "slide",
+      controlNav: false,
+      animationLoop: false,
+      slideshow: false,
+      itemWidth: 210,
+      itemMargin: 5,
+      asNavFor: '#slider'
+    });
+   
+    $('#slider').flexslider({
+      animation: "slide",
+      controlNav: false,
+      animationLoop: false,
+      slideshow: false,
+      sync: "#carousel"
+    });
+  });
+
+(function($) {
+    $(function() {
+      $("ul.tabs__caption").on("click", "li:not(.active)", function() {
+        $(this)
+          .addClass("active")
+          .siblings()
+          .removeClass("active")
+          .closest("div.tabs")
+          .find("div.tabs__content")
+          .removeClass("active")
+          .eq($(this).index())
+          .addClass("active");
+      });
+    });
+})(jQuery);
+
+function myFunction() {
+  var x = document.getElementById("nav");
+  if (x.className === "nav") {
+      x.className += " responsive";
+  } else {
+      x.className = "nav";
+  }
+}
